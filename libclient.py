7628bcae3cb785fa8cc3cad7502ba02458e952b0
JPimia <90615229+JPimia@users.noreply.github.com>
2024-02-20 14:10:06 +0200

    Add request queuing and response processing methods

--- libclient.py
+++ libclient.py
@@ -22,4 +22,38 @@ class Message:
                 # set selector to listen read events
                 self._set_selector_events_mask("r")
                 
+    def queue_request(self):
+        content = self.request["content"]
+        content_type = self.request["type"]
+        content_encoding = self.request["encoding"]
+        if content_type == "text/json":
+            req = {
+                "content_bytes": self._json_encode(content, content_encoding),
+                "content_type": content_type,
+                "content_encoding": content_encoding,
+            }
+        else:
+            req = {
+                "content_bytes": content,
+                "content_type": content_type,
+                "content_encoding": content_encoding,
+            }
+        message = self._create_message(**req)
+        self._send_buffer += message
+        self._request_queued = True
         
+    def process_response(self):
+
+        
+        self.close()
+        
+    def _read(self):
+        try:
+            data = self.sock.recv(4096)
+        except BlockingIOError:
+            pass
+        else:
+            if data:
+                self._recv_buffer += data
+            else:
+                raise RuntimeError("Peer closed.")
